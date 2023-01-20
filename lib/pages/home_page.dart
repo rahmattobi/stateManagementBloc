20b4c19a64e9865e290280379ae9437250e22ba5
rahmattobi <tobytoby298@gmail.com>
2023-01-20 21:21:40 +0700

    blocBuilder

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/counter.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Counter myCounter = Counter();
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Bloc Cubik'),
+        centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          BlocBuilder<Counter, int>(
+            bloc: myCounter,
+            builder: (context, state) {
+              return Text(
+                '$state',
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              );
+            },
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              IconButton(
+                icon: const Icon(Icons.remove),
+                onPressed: () {
+                  myCounter.decrement();
+                },
+              ),
+              IconButton(
+                icon: const Icon(Icons.add),
+                onPressed: () {
+                  myCounter.increment();
+                },
+              ),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
